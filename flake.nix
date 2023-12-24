a15a2e62641ebbb12c1e4db023dd223dcfa5e60e
Ryan Mulligan <ryan@ryantm.com>
2023-12-23 21:11:30 -0800

    test

--- flake.nix
+++ flake.nix
@@ -10,13 +10,30 @@
     rev = "0.0.0-${builtins.substring 0 7 long_rev}";
 
     eachSystem = nixpkgs.lib.genAttrs (import systems);
-    versionix = drv: nixpkgs.legacyPackages.${drv.system}.writeShellApplication {
+    versionix = drv: let
+      pkgs = nixpkgs.legacyPackages.${drv.system};
+    in (pkgs.writeShellApplication {
       name = drv.name;
       text = ''
         export VERSION="${rev}"
         ${drv}/bin/print-version
       '';
-    };
+    }).overrideAttrs (_: _: {
+      passthru = {
+        inherit drv;
+        didChange = pkgs.writeShellApplication {
+          name = "did-change";
+          runtimeInputs = [ pkgs.nix ];
+          text = ''
+            PREV_REV=$1
+            PREV_OUTPATH=$(nix eval ".?rev=''${PREV_REV}#print-version.drv.outPath")
+            CUR_OUTPATH=${drv.outPath}
+            echo "$PREV_OUTPATH"
+            echo "$CUR_OUTPATH"
+          '';
+        };
+      };
+    });
   in {
     formatter = eachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
 
